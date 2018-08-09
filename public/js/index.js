2a3acb04f7361ce2a50255adcffdb1e10e58d28c
ShahafShuhamy <Shahaf761@gmail.com>
2018-08-09 09:28:14 +0300

    add new message and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,29 @@
+var socket = io();
+socket.on('connect', function () {
+
+    //fire a custom event to Server
+    socket.emit('createEmail',{
+        to:'email@example',
+        text:'Hey.this is me'
+    });
+
+    socket.emit('createMessage',{
+        from:'email@example',
+        text:'Hey.this is me'
+    });
+
+    console.log('connected to server');
+});
+
+socket.on('disconnect', function ()  {
+    console.log('Disconnected from Server');
+});
+
+socket.on('newMessage', function (message) {
+    console.log(message);
+});
+
+//listenning to a custom event from server
+socket.on('newEmail',function (email) {
+    console.log('new email ',email);   
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,19 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) =>{
     console.log('new user connected');
+    //fires a custom event to Client
+
+    socket.emit('newMessage',{
+        from:'remote User',
+        text:"hi there i'm remote user",
+        createdAt:'09/08/2018'
+    });
+
+    //listenning to a custom event from client
+    socket.on('createMessage', (newMessage) =>{
+        console.log('create message',newMessage);
+    });
+
     socket.on('disconnect',() =>{
         console.log('user has been disconnected');
     });
