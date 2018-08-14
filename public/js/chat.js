3880bc3fb38a6018a3a6f01e572952d3b905b300
ShahafShuhamy <Shahaf761@gmail.com>
2018-08-14 11:14:23 +0300

    send messages to only ppl in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -111,7 +111,6 @@ socket.on('newLocationMessage',function(message){
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
     socket.emit('createMessage',{
-        from:'User',
         text: jQuery('[name=message]').val()
     },function () {
         jQuery('[name=message]').val("");
